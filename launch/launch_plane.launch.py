a854283f1cb811bd570d095044d1f9a48393c800
attu0 <atharvmudse@gmail.com>
2026-03-19 15:19:18 +0530

    fixed lauch sim

--- launch/launch_plane.launch.py
+++ launch/launch_plane.launch.py
@@ -1,7 +1,7 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
@@ -12,10 +12,14 @@ def generate_launch_description():
 
     pkg_share = get_package_share_directory('imu_plane')
 
-    # Launch argument
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Include rsp.launch.py
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation time'
+    )
+
     rsp_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_share, 'launch', 'rsp.launch.py')
@@ -23,7 +27,6 @@ def generate_launch_description():
         launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
-    # Include imu.launch.py
     imu_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_share, 'launch', 'imu.launch.py')
@@ -31,6 +34,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        declare_use_sim_time,
         rsp_launch,
         imu_launch
     ])
